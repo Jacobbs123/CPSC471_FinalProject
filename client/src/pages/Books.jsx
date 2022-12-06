59302f2ae9986bf9cbccae078d654c3af4513916
Jacobbs123 <jacobschon01@gmail.com>
2022-12-06 13:35:41 -0700

    init

---
+++ client/src/pages/Books.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+const Books = () => {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        const fetchAllBooks = async ()=> {
+            try {
+                // Makes an API request to this link
+                const res = await axios.get("http://localhost:8800/books");
+                setBooks(res.data);
+                console.log(res);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        fetchAllBooks()
+    }, []);
+    return (
+        <div>
+            <h1>UCalgary Pet Store</h1>
+            <div className="books">
+                {books.map(book=>(
+                    <div className="book" key={book.id}>
+                        {book.cover && <img src={book.cover } alt="" />}
+                        <h2>{book.title}</h2>
+                        <p>{book.desc}</p>
+                        <span>{book.price}</span> 
+                        <button className="delete">Delete</button>
+                        <button className="update">Update</button> 
+                        </div>
+                ))}
+            </div>
+            <button><Link to="/add">Add new book</Link></button> 
+        </div>
+    )
+}
+export default Books;
