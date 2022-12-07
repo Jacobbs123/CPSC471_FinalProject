b55b5d1a47f09c068baf5dae631b36731505c01b
Jacobbs123 <jacobschon01@gmail.com>
2022-12-06 23:24:48 -0700

    added pages as well as created database connection

---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import {React, useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Homepage = () => {
+    return (
+        <div><h1 className="text-6xl">Welcome to our Ucalgary petstore</h1>
+        <p className="text-2xl">Please <Link to="/login">Sign in</Link> to continue</p></div>
+    )
+}
+export default Homepage;
